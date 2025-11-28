540424b9f9879d946e36ccdba4e71c0e71b5ecea
IgorCeola <igorceola@hotmail.com>
2025-11-28 16:35:07 -0300

    Adicionando biblioteca httpx no arquivo requirements.txt

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -2,6 +2,8 @@ from repository.book_repository import BookRepository
 from domain.book import Book
 from unittest.mock import MagicMock
 
+VALID_ID = "507f1f77bcf86cd799439011"
+
 def test_listar():
     repo = BookRepository()
     repo_collection = MagicMock()
@@ -16,17 +18,22 @@ def test_listar():
     assert result[0]["id"] == "123"
 
 def test_buscar_por_id():
-    repo = BookRepository()
-    repo_collection = MagicMock()
-    repo_collection.find_one.return_value = {
-        "_id": "123", "titulo": "A"
-    }
+        repo = BookRepository()
+        repo_collection = MagicMock()
+        
+        repo_collection.find_one.return_value = {
+            "_id": ObjectId(VALID_ID),
+            "titulo": "A"
+        }
 
-    import repository.book_repository as module
-    module.books_collection = repo_collection
+        import repository.book_repository as module
+        module.books_collection = repo_collection
 
-    result = repo.buscar_por_id("123")
-    assert result["id"] == "123"
+        result = repo.buscar_por_id(VALID_ID)
+        
+        assert result["id"] == VALID_ID 
+
+        repo_collection.find_one.assert_called_once_with({"_id": ObjectId(VALID_ID)})
 
 def test_adicionar():
     repo = BookRepository()
