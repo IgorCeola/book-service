2c5946f5fa1fadad18153bd96414836078a00085
IgorCeola <igorceola@hotmail.com>
2025-11-28 15:35:11 -0300

    Adicionando inicialização do Prometheus

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI
 from controllers.book_controller import router
-# import threading 
-# from consumer.book_validation_consumer import start_consumer
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 app = FastAPI(title="Book Service")
 
 app.include_router(router)
 
+@app.on_event("startup")
+def startup_event():
+    Instrumentator().instrument(
+        app, 
+        exclude_paths=["/metrics"], 
+    ).expose(app)
+
 @app.get("/")
 def root():
     return {"mensagem": "Book Service ativo, Capitão!"}
 
-
-# def start_background():
-#     thread = threading.Thread(target=start_consumer)
-#     thread.daemon = True
-#     thread.start()
-
-# start_background()
